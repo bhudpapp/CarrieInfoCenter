8be2bdde5e28ade03e6ad952d2ea03fcef904cd0
Daniel Papp <daniel.papp@flex.com>
2018-01-10 07:50:07 -0800

    [Studio] Modified screen "SearchResult"

--- client/controllers/app/searchresult.js
+++ client/controllers/app/searchresult.js
@@ -2,7 +2,7 @@ angular.module('app').controller('app_searchresult', app_searchresult);
 function app_searchresult($scope, app) {
     'use strict';
     app.init($scope);
-};
+}
 $scope.gohome = function()
 {
     app.go('app.landing');
