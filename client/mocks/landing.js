938b75d0bee8b12611c465a54bf154a4c9230238
Daniel Papp <daniel.papp@flex.com>
2018-01-10 07:20:43 -0800

    [Studio] Update mock actions for "landing"

--- client/mocks/landing.js
+++ client/mocks/landing.js
@@ -2,5 +2,5 @@
 window.mocks = window.mocks || {};
 window.mocks.landing = window.mocks.landing || {};
 window.mocks.landing.search = function ($scope, params, app) {
-    app.go('app.SearchResult');
+    app.go('app.searchresult');
 };
