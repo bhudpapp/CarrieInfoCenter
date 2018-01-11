cea5230c950ef1fb389bfae7ebcbf52eb7adddfc
Daniel Papp <daniel.papp@flex.com>
2018-01-11 02:13:43 -0800

    [Studio] Delete screen model "genericinformation", Delete screen model "shipmentdetail_test", Delete screen model "searchresult"

--- client/mocks/searchresult.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-window.mocks = window.mocks || {};
-window.mocks.searchresult = window.mocks.searchresult || {};
-window.mocks.searchresult.listItems_searchResultClick = function ($scope, params, app) {
-    app.go('genericinformation');
-};
--- client/mocks/shipmentdetail_test.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-window.mocks = window.mocks || {};
-window.mocks.shipmentdetail_test = window.mocks.shipmentdetail_test || {};
