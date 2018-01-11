4c20fa11b1958429f75d2bb5e11484e171456504
Daniel Papp <daniel.papp@flex.com>
2018-01-11 00:35:56 -0800

    [Studio] Update mock actions for "searchresult"

--- client/mocks/searchresult.js
+++ client/mocks/searchresult.js
@@ -1,3 +1,6 @@
 'use strict';
 window.mocks = window.mocks || {};
 window.mocks.searchresult = window.mocks.searchresult || {};
+window.mocks.searchresult.listItems_searchResultClick = function ($scope, params, app) {
+    app.go('shipmentdetail');
+};
