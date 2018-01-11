41855286397ef7dcc1773cc8375fc4ca594fb5e3
Daniel Papp <daniel.papp@flex.com>
2018-01-11 01:15:38 -0800

    [Studio] Add screen "ShipmentDetail2"

---
+++ client/controllers/app/shipmentdetail2.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_shipmentdetail2', app_shipmentdetail2);
+function app_shipmentdetail2($scope, app) {
+    'use strict';
+    app.init($scope);
+}
---
+++ client/mocks/shipmentdetail2.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.shipmentdetail2 = window.mocks.shipmentdetail2 || {};
