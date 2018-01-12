56156db9feec641a0f0d45ba974e14aac67a7c0e
Daniel Papp <daniel.papp@flex.com>
2018-01-12 00:53:52 -0800

    [Studio] Modified screen "GenericInformation"

--- client/controllers/app/genericinformation.js
+++ client/controllers/app/genericinformation.js
@@ -3,9 +3,13 @@ function app_genericinformation($scope, app) {
     'use strict';
     app.init($scope);
     
-    app.data.isGenericInformation = false;
+    app.data.isGenericInformation = true;
     
     $scope.back = function() {
         page.history(-1);
+    };
+    
+    $scope.changeScreen = function() {
+        app.data.isGenericInformation = !app.data.isGenericInformation;
     };
 }
